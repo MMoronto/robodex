508f63a56eaf77780c8a951aa1d37ebf74b7177b
MMORONTO <t2wycemo@gmail.com>
2021-07-26 23:12:41 -0400

    refactor card component. destructure props

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Card = (props) => {
-	const { name, email, id } = props;
+const Card = ({ name, email, id }) => {
+
 	return (
 			<div className='tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
 				<h1>Robodex</h1>
 				<h3>Rolodex for your robot buddies</h3>
 
-				<img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
+				<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
 				<div>
-					<h2>{props.name}</h2>
-					<p>{props.email}</p>
+					<h2>{name}</h2>
+					<p>{email}</p>
 				</div>
 			</div>
 		);
