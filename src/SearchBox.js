7825fe87f275ac051b5ed3a062b5fc603564fec6
MMORONTO <t2wycemo@gmail.com>
2021-07-27 12:39:46 -0400

    style searchbox. use tachyons

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const SearchBox = () => {
     return (
-        
-    )
+        <div className='pa2'>
+            <input 
+            className='pa3 ba b--light-red bg-light-yellow'
+                type='search' 
+                placeholder='search robots' 
+            />
+        </div>
+    );
 }
+
+export default SearchBox;
