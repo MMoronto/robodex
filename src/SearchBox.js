89f4488e972d6a4a183f6b1a095bdda8eb4d08ce
MMORONTO <t2wycemo@gmail.com>
2021-07-27 16:02:46 -0400

    make search field interactive

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className='pa2'>
             <input 
-            className='pa3 ba b--light-red bg-light-yellow'
+                className='pa3 ba b--light-red bg-light-yellow'
                 type='search' 
                 placeholder='search robots' 
+                onChange={searchChange}
             />
         </div>
     );
