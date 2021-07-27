2b91bc1088834299c551684dd77b75687f7e3a50
MMORONTO <t2wycemo@gmail.com>
2021-07-26 22:56:12 -0400

    add robot.js to src folder

---
+++ src/robot.js
@@ -0,0 +1,26 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Ventress Caymus',
+		username: "Glom",
+		email: 'Douzieme@gazette.biz' 
+	},
+	{
+		id: 2,
+		name: 'Bunions Lorzins',
+		username: "Rube",
+		email: 'Kalash@ant.org' 
+	},
+	{
+		id: 3,
+		name: 'Forex Deviouss',
+		username: "Drab",
+		email: 'Panettiere@sus.biz' 
+	},
+	{
+		id: 4,
+		name: 'Fred Jons',
+		username: "Fredo",
+		email: 'ffj@danube.net' 
+	}
+]
